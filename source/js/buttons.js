20dfa23927fb29d82095b53b5167d3a315d95352
elliotdkim <57509499+elliotdkim@users.noreply.github.com>
2021-02-10 20:55:12 -0800

    Update buttons.js

--- source/js/buttons.js
+++ source/js/buttons.js
@@ -1,5 +1,17 @@
-var start_time = 25;
-var countDownDate = new Date()
+// listener for start button
+//      on "click"
+//          call function in controller
+//              function sets cycle to pomo (pomo started)
+//                  that function could call timer module with (time, running = 1)
+//
+// listener for stop button
+//      on "click"
+//          call function in controller
+//              function resets cycle
+//                  that function sets running = 0
+
+
+
 
 // button is used to disable the buttons 
 const button = document.querySelectorAll("button"); // 0 is help, 1 is start, 2 is stop
@@ -10,14 +22,24 @@ var stop = document.getElementById("stop-button");
 
 
 // POMO GO
-start.addEventListener("click", startTimer);
+start.addEventListener("click", function() {
+    startTimer;
+    button[1].disabled = true;      // Disable start button
+    button[2].disabled = false;     // Enable stop button
+});
 
 // POMO STAHP
-stop.addEventListener("click", stopTime);
+stop.addEventListener("click", function (){
+    stopTimer;
+    button[2].disabled = true;      // Disable stop button
+    button[1].disabled = false;     // Enable start button
+});
 
-/**
- * test
- */
+/** Following functionality will be in conroller.js and timer.js
+
+var start_time = 25;
+var countDownDate = new Date()
+
 function startTimer() {
     button[1].disabled = true;
     var startTime = Date.now();
@@ -36,18 +58,6 @@ function stopTime() {
     clearInterval(timerBegins);
     button[1].disabled = false;
 }
+ */
 
 
-
-
-// listener for start button
-//      on "click"
-//          call function in controller
-//              function sets cycle to pomo (pomo started)
-//                  that function could call timer module with (time, running = 1)
-//
-// listener for stop button
-//      on "click"
-//          call function in controller
-//              function resets cycle
-//                  that function sets running = 0
