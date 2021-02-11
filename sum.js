5fe74365900ba8c27564b4b021e12b27efa230ad
Wilson-Low <60717453+Wilson-Low@users.noreply.github.com>
2021-02-10 23:46:54 -0800

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,3 +1,9 @@
+/**
+ * Sums two numbers together
+ * @param a - first number to be added
+ * @param b - second number to be added
+ * @returns {number} - the sum
+ */
 function sum(a, b) {
   return a + b;
 }
