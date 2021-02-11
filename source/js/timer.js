4ce51876de6270e91d1504a4714e96dc1ae154d1
elliotdkim <57509499+elliotdkim@users.noreply.github.com>
2021-02-10 19:28:57 -0800

    Update timer.js

--- source/js/timer.js
+++ source/js/timer.js
@@ -1,1 +1,5 @@
-
+// Main timer component:
+//    variable that keeps track of time elapsed
+//    Function that changes the static html page (to be in sync with the actual timer)
+//    reset function, input time in minutes to count down
+//    when timer is finished, call method in controller (tbd)
