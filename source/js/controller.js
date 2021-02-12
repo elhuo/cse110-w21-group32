0ee0bffe6d0cc0bd9879c6cd1ea04f858976241a
jpwong0316 <47432273+jpwong0316@users.noreply.github.com>
2021-02-11 19:37:17 -0800

    updated controller.js to call timer functions

--- source/js/controller.js
+++ source/js/controller.js
@@ -1,5 +1,3 @@
-// import from timer.js
-
 // tracks the type of cycle
 // 0 = pomo, 1 = short break, 2 = long break
 var cycle = 0;
@@ -8,17 +6,19 @@ var cycle = 0;
 // long break when numPomos = 4
 var numPomos = 0;
 
-// value is 1 when timer is running, 0 when stopped
-// might not need this
-// var timerRunning = 0;
+// 25 - 1 minutes to pass in to startCountdown for pomodoro cycle
+const pomoTime = 24;
+// 5 - 1 minutes to pass in to startCountdown for short break cycle
+const sBreakTime = 4;
+// 15 - 1 minutes to pass in to startCountdown for long break cycle
+const lBreakTime = 14;
 
 // probably called by the buttons.js event listener for start
 function startTimer() {
     cycle = 0;
     numPomos = 0;
     changeStyle();
-    // timerRunning = 1;
-    // call timer start func, needs to pass in current cycle? ex. timer(cycle)
+    startCountdown(pomoTime);
 }
 
 // probably called by the buttons.js event listener for stop
@@ -26,36 +26,40 @@ function stopTimer() {
     cycle = 0;
     numPomos = 0;
     changeStyle();
-    // timerRunning = 0;
-    // call timer stop func, needs to pass in current cycle? ex. timer(cycle)
+    stopCountdown();
 }
 
 // when timer countdown ends, call this function
-export function changeCycles() {
+function changeCycles() {
     // if current cycle is pomo, increment numPomos
     if (cycle == 0) numPomos++;
+    
     // if current cycle is pomo and 4 pomos haven't occurred yet,
     // set cycle to short break
     if (cycle == 0 && numPomos < 4) {
         cycle = 1;
-        // pass start time or cycle type to timer? timer("5:00")
+        startCountdown(sBreakTime);
     }
+    
     // if current cycle is pomo and 4 pomos have occurred,
     // set cycle to long break and reset numPomos
     else if (cycle == 0 && numPomos == 4) {
         numPomos = 0;
         cycle = 2;
-        // pass start time or cycle type to timer? timer("15:00")
+        startCountdown(lBreakTime);
     }
+    
     // after short and long breaks, return to pomo cycle
     else if (cycle == 1) {
         cycle = 0;
-        // pass start time or cycle type to timer? timer("25:00")
+        startCountdown(pomoTime);
     }
     else if (cycle == 2) {
         cycle = 0;
-        // pass start time or cycle type to timer? timer("25:00")
+        startCountdown(pomoTime);
     }
+    
+    // change page style according to new cycle
     changeStyle();
 }
 
