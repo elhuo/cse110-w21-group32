bcc7232be424dc0ba677a6c207fe8ca077d4dd9c
Wilson-Low <60717453+Wilson-Low@users.noreply.github.com>
2021-02-22 13:00:41 -0800

    Update controller.test.js

--- source/js/controller.test.js
+++ source/js/controller.test.js
@@ -1,12 +1,15 @@
+/** loading the module we're testing */
 const controller = require("./controller");
 
+/** creating jest mocks (because loading module doesn't load dependencies) */
 document.body.innerHTML =
     "<div id='pomo-tab'></div>" +
     "<div id='short-break-tab'></div>" +
     "<div id='long-break-tab'></div>";
 startCountdown = jest.fn();
+
+/** Testing startTimer function */
 test('startTimer test', () => {
-    const startCountdown = jest.fn();
     controller.startTimer();
     expect(controller.cycle).toBe(0);
     expect(controller.numPomos).toBe(0);
