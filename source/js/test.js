f3a8a759df3f10808b637ab2e57e8cde84a471fe
elhuo <elhuo@ucsd.edu>
2021-03-16 15:46:26 -0700

    Update test.js

--- source/js/test.js
+++ source/js/test.js
@@ -1,1 +1,8 @@
-console.log("HelloWorld")
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {
+}
